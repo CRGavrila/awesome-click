730c40287e27abf6a17140a6b80a40da24e1c048
crgavrila <c_r_gavrila@yahoo.com>
2019-01-04 08:54:39 +0200

    added click events for canvas

---
+++ particles.js
@@ -0,0 +1,91 @@
+const PARTICLE_COUNT = 1000;
+
+async function initState() {
+    const f32a = new Float32Array(PARTICLE_COUNT * 4);
+
+    function fire(x, y) {
+        const len = PARTICLE_COUNT * 4;
+        for(let ptr = 0; ptr < len; ptr +=4) {
+            f32a[ptr] = x;
+            f32a[ptr + 1] = y;
+            f32a[ptr + 2] = Math.random()*20 - 10;
+            f32a[ptr + 3] = Math.random()*20 - 10;
+        }
+    }
+
+    function physics() {
+        const len = PARTICLE_COUNT * 4;
+        for(let ptr = 0; ptr < len; ptr +=4) {
+            f32a[ptr] += f32a[ptr + 2];
+            f32a[ptr + 1] += f32a[ptr + 3];
+            f32a[ptr + 3] += 0.25;
+        }
+    }
+
+    function buffer() {
+        return f32a;
+    }
+
+    return { fire, physics, buffer };
+};
+
+async function initView(canvas) {
+    const ctx = canvas.getContext('2d');
+
+    function render(f32a) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#ffffff';
+        ctx.save();
+        ctx.translate(canvas.width/2, canvas.height/2);
+        const len = PARTICLE_COUNT * 4;
+        for(let ptr = 0; ptr < len; ptr +=4) {
+            ctx.fillRect(
+                f32a[ptr], f32a[ptr + 1],
+                2, 2
+            )
+        }
+
+        ctx.restore();
+    }
+
+    function resize(width, height) {
+        canvas.width = width;
+        canvas.height = height;
+
+    }
+
+    return { render, resize }
+}
+
+async function main() {
+
+    const canvas = document.body.appendChild(document.createElement('canvas'));
+    const [ state, view ] = await Promise.all([
+        initState(),
+        initView(canvas),
+    ]);
+
+    function onResize() {
+        view.resize(canvas.clientWidth, canvas.clientHeight);
+    }
+
+    function onClick(e) {
+        state.fire(
+            e.offsetX - canvas.clientWidth / 2,
+            e.offsetY - canvas.clientHeight / 2
+        );
+    }
+
+    canvas.addEventListener('click', onClick);
+    window.addEventListener('resize', onResize);
+    onResize();
+
+    (function frame() {
+        state.physics();
+        view.render(state.buffer());
+        requestAnimationFrame(frame);
+    })();
+
+}
+
+main();
